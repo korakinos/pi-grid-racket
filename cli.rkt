9e57802c5746264ad9450007514b2e74173fc198
Carlos Franke <carlos_franke@lemtank.de>
2016-06-11 17:07:10 +0200

    Workable version

--- cli.rkt
+++ cli.rkt
@@ -2,11 +2,30 @@
 #lang racket/base
 
 (require "functions.rkt")
+(require racket/cmdline)
 
-(display "Testläufe mit n=3000:\n")
-(display "volloptimiert:\n")
-(time (exact->inexact (pi-approx-opt2 3000)))
-(display "etwas optimiert:\n")
-(time (exact->inexact (pi-approx-opt1 3000)))
-(display "nicht optimiert:\n")
-(time (exact->inexact (pi-approx      3000))) 
+(define n (make-parameter 10))
+(define optimize-level (make-parameter 2))
+
+(command-line
+  #:once-each
+  [("-n" "--number") number "Gitter-Seitenlänge"
+                     (n (string->number number))]
+  #:once-any
+  [("--optimize-0") "ohne Optimierung"
+                          (optimize-level 0)]
+  [("--optimize-1") "leichte Optimierung"
+                          (optimize-level 1)]
+  [("--optimize-2") "volle Optimierung"
+                          (optimize-level 2)])
+
+(printf "n=~a~n" (n))
+(cond [(= (optimize-level) 2)
+       (display "Volloptimiert:\n")
+       (time (exact->inexact (pi-approx-opt2 (n))))]
+      [(= (optimize-level) 1)
+       (display "Etwas optimiert:\n")
+       (time (exact->inexact (pi-approx-opt1 (n))))]
+      [(= (optimize-level) 0)
+       (display "Nicht optimiert:\n")
+       (time (exact->inexact (pi-approx (n))))])
--- functions.rkt
+++ functions.rkt
@@ -41,17 +41,18 @@
 ; Zählt nur an der Kreislinie entlang statt über die ganze Fläche, dadurch O(n) statt O(n²).
 (define (pi-approx-opt2 number)
   (define (dots-in-circle n)
+    (define (x-loop x y)
+      (if (< (sqrt (+ (* x x) (* y y)))
+             n)
+        x
+        (x-loop (- x 1) y)))
     (let y-loop ([y 0]
                  [start-x n]
                  [counter 0])
       (if (= y n)
         counter
         (let ([found-x 
-                (+ 1 (let x-loop ([x start-x])
-                       (if (< (sqrt (+ (* x x) (* y y)))
-                              n)
-                         x
-                         (x-loop (- x 1)))))])
+                (+ 1 (x-loop start-x y))])
           (y-loop (+ y 1)
                   found-x
                   (+ counter
